a87d1d9eeca118b271aafee7cd077f31462c63fb
Jihyeon Ha <69135462+haaforever@users.noreply.github.com>
2020-09-22 16:09:44 +0900

    Update MonteCarlo.R

--- MonteCarlo.R
+++ MonteCarlo.R
@@ -3,12 +3,13 @@
 # Optimal hyperplane
 # x_1 + ... + x_d = 0.5 * d * mu
 fn.opt.hyppln <- function(x, d, mu) {
-  if (sum(x) - (0.5 * d * mu) >= 0) {
-    y <- 1
-  } else {
-    y <- 0
-  }
-  return(y)
+  # if (sum(x) - (0.5 * d * mu) >= 0) {
+  #   y <- 1
+  # } else {
+  #   y <- 0
+  # }
+  # return(y)
+  return(sum(x) - (0.5 * d * mu))
 }
 
 
@@ -19,21 +20,30 @@ fn.opt.hyppln <- function(x, d, mu) {
 # Fixed mean vector of first Gaussian: mu1 = (0, ..., 0)
 # mu2 = (mu, ..., mu)
 library(mvtnorm)
+library(ROCR)
 d <- c(5, 10 * (1:10))
-n <- 1000000
+n <- 10000
 mu <- 0.05 * (1:20)
 auc <- matrix(data = 0, nrow = length(mu), ncol = length(d))
 rownames(auc) <- mu
 colnames(auc) <- d
-for (i.d in 1:length(d)) {
-  for (i.mu in 1:length(mu)) {
-    x.0 <- rmvnorm(n = n, mean = rep(0, d[i.d]), sigma = diag(1, d[i.d]))
-    x.1 <- rmvnorm(n = n, mean = rep(mu[i.mu], d[i.d]), sigma = diag(1, d[i.d]))
-    y.0 <- apply(X = x.0, MARGIN = 1, FUN = fn.opt.hyppln, mu = mu[i.mu], d = d[i.d])
-    y.1 <- apply(X = x.1, MARGIN = 1, FUN = fn.opt.hyppln, mu = mu[i.mu], d = d[i.d])
-    auc[i.mu, i.d] <- 0.5 * ((sum(y.0 == 0) / n) + (sum(y.1 == 1) / n))
+n.rep <- 100
+for (i in 1:n.rep) {
+  for (i.d in 1:length(d)) {
+    # i.d <- 1
+    for (i.mu in 1:length(mu)) {
+      # i.mu <- 1
+      x.0 <- rmvnorm(n = n, mean = rep(0, d[i.d]), sigma = diag(1, d[i.d]))
+      x.1 <- rmvnorm(n = n, mean = rep(mu[i.mu], d[i.d]), sigma = diag(1, d[i.d]))
+      y.0 <- apply(X = x.0, MARGIN = 1, FUN = fn.opt.hyppln, mu = mu[i.mu], d = d[i.d])
+      y.1 <- apply(X = x.1, MARGIN = 1, FUN = fn.opt.hyppln, mu = mu[i.mu], d = d[i.d])
+      # auc[i.mu, i.d] <- 0.5 * ((sum(y.0 == 0) / n) + (sum(y.1 == 1) / n))
+      auc[i.mu, i.d] <- auc[i.mu, i.d] + performance(prediction.obj = prediction(predictions = c(y.0, y.1), labels = c(rep(-1, n), rep(1, n))),
+                                    measure = "auc")@"y.values"[[1]]
+    }
+    print(d[i.d])
   }
-  print(d[i.d])
+  print(paste0(i, "-th iteration"))
 }
-auc
+auc <- auc / n.rep
 
